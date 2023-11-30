990330c26650d600cfb9294d20b422dd19893b82
Manvir Singh Heer <manvirheer007@gmail.com>
2023-11-30 13:57:38 -0800

    bug: fix npm run build issues with ' rules

--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -1,14 +1,6 @@
 
 // Import necessary modules and components
-import { PortableText } from '@portabletext/react'
-import Container from '~/components/Container'
-import AboutSectionTextContent from '~/components/home/aboutSectionTextContent';
-import WorkWithUs from '~/components/home/WorkWithUs';
-import HomePageComponent from '~/components/home/HomePage';
 import ContactPageComponent from '~/components/contact/ContactPageComponent';
-
-
-
 
 // Homepage component
 export default function ContactPage() {
--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,21 +1,10 @@
 // Import necessary modules and components
-import { PortableText } from '@portabletext/react'
-import Container from '~/components/Container'
-import AboutSectionTextContent from '~/components/home/aboutSectionTextContent';
-import WorkWithUs from '~/components/home/WorkWithUs';
 import HomePageComponent from '~/components/home/HomePage';
-import ContactPage from '~/components/contact/ContactPageComponent';
-
-
-
 
 // Homepage component
 export default function HomePage() {
   return (
     <>
-        {/* <ImageSlider />
-        < AboutSectionTextContent />
-        <WorkWithUs /> */}
         <HomePageComponent />
     </>
   )
